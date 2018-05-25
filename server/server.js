2cdd6c18d5eed3e718a8a874bb65f1b8267489d6
nhat thanh <nhatthanhlolo1@gmail.com>
2018-05-26 00:06:13 +0700

    add new route /

--- server/server.js
+++ server/server.js
@@ -3,4 +3,9 @@ const express = require('express')
 const port = process.env.port || 5000
 
 const app = express()
+
 app.listen(port)
+
+app.get("/",(req,res)=>{
+    res.status(200).send({hi:"there"})
+})
