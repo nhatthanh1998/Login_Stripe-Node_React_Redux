aac6def6ea81e544b07fa3b876b9c05fe7f30292
nhat thanh <nhatthanhlolo1@gmail.com>
2018-05-26 00:19:02 +0700

    fix bug v1.01

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
-const port = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 const app  = express()
 app.get("/", (req,res)=>{
     res.status(200).send({hi:"there"})
 })
+
+app.listen(PORT)
