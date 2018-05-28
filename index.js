652a9ed2c4b3e2ba6d13ef6c2b6a99e94f65a404
nhat thanh <nhatthanhlolo1@gmail.com>
2018-05-29 05:34:54 +0700

    new version

--- index.js
+++ index.js
@@ -20,13 +20,7 @@ app.use(passport.session())
 require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('/client/build'))
-    const path = require('path')
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
-}
+app.use(express.static(__dirname+'/client/build'))
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
